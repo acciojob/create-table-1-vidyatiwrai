2726ace671a403577ef1306a53dc53fbc10f4828
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-06-15 12:21:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+// script.js
+
 function insert_Row() {
-    //Write your code here
-  
-  
+  const table = document.getElementById('sampleTable');
+  const newRow = table.insertRow(0); // Insert row at the top
+
+  // Create the left cell and set its content
+  const leftCell = newRow.insertCell(0);
+  leftCell.textContent = 'New Cell1';
+
+  // Create the right cell and set its content
+  const rightCell = newRow.insertCell(1);
+  rightCell.textContent = 'New Cell2';
 }
+
